a326d6e90562063b8644908a0f64f18136f1f873
Filimoncev <ilya.filimoncev@mail.ru>
2025-09-19 17:21:54 +0700

    В проект добавлен пакет table_calendar. Реализован функционал кнопки календаря. Добавлена вёрстка календаря.

--- lib/ui/screens/dashboard_screen.dart
+++ lib/ui/screens/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:first_flutter_project/ui/shared/taska_text_form_field.dart';
 import 'package:first_flutter_project/ui/shared/taska_title_text.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class DashboardScreen extends StatefulWidget {
   DashboardScreen({Key? key});
@@ -13,9 +14,86 @@ class DashboardScreen extends StatefulWidget {
 const myThemeColor = Color(0xFF665EE2);
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  //Календарь
+  final GlobalKey _scaffoldKey = GlobalKey();
+  OverlayEntry? _overlayEntry;
+  void _showOverlay(BuildContext context) {
+    OverlayState? overlayState = Overlay.of(_scaffoldKey.currentContext!);
+    _overlayEntry = OverlayEntry(
+      builder: (context) => Stack(
+        children: [
+          GestureDetector(
+            onTap: () => _hideOverlay(_overlayEntry!),
+            child: Container(
+              color: Colors.black.withValues(alpha: 0.5),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+            ),
+          ),
+          Center(
+            child: Material(
+              elevation: 8.0,
+              borderRadius: BorderRadius.circular(16.0),
+              child: Container(
+                width: 300,
+                height: 340,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                child: TableCalendar(
+                  calendarStyle: CalendarStyle(
+                    todayTextStyle: TextStyle(color: myThemeColor),
+                    todayDecoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      border: Border.all(color: myThemeColor),
+                    ),
+                  ),
+                  firstDay: DateTime(2010, 1, 1),
+                  lastDay: DateTime(2030, 1, 1),
+                  focusedDay: DateTime.now(),
+                  rowHeight: 40,
+                  headerStyle: HeaderStyle(
+                    formatButtonVisible: false,
+                    titleCentered: true,
+                    leftChevronIcon: Icon(Icons.arrow_back),
+                    rightChevronIcon: Icon(Icons.arrow_forward),
+                  ),
+                  calendarBuilders: CalendarBuilders(
+                    headerTitleBuilder: (context, date) {
+                      return Text(
+                        style: TextStyle(
+                          color: myThemeColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                        DateFormat('MMMM').format(date),
+                        textAlign: TextAlign.center,
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    overlayState.insert(_overlayEntry!);
+  }
+
+  void _hideOverlay(OverlayEntry entry) {
+    entry.remove();
+  }
+
+  //Основная вёрстка
   @override
   Widget build(BuildContext context) {
+    final DateTime now = DateTime.now();
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         toolbarHeight: 80,
         automaticallyImplyLeading: false,
@@ -24,7 +102,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
         backgroundColor: Color(0xFFF8F7FD),
       ),
       body: Column(
-        //mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Padding(
             padding: EdgeInsetsGeometry.all(20),
@@ -38,9 +115,17 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   topText: "20 октября",
                   bottomText: "3 задачи на сегодня",
                 ),
-
                 Spacer(),
-                Icon(Icons.calendar_month),
+                IconButton(
+                  onPressed: () => _showOverlay(context),
+                  icon: Icon(Icons.calendar_month),
+                  iconSize: 30,
+                  style: IconButton.styleFrom(
+                    side: BorderSide(color: Color(0xFFE7EFFD)),
+                    shape: CircleBorder(),
+                    backgroundColor: Colors.transparent,
+                  ),
+                ),
               ],
             ),
           ),
@@ -57,7 +142,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                     width: 60,
                     margin: EdgeInsets.symmetric(horizontal: 8),
                     decoration: BoxDecoration(
-                      //  border: Border.all(color: Color(0xFFE7EFFD)),
+                      border: Border.all(color: Color(0xFFE7EFFD)),
                       borderRadius: BorderRadius.circular(18),
                       color: Color(0xFFF8F7FD),
                     ),
