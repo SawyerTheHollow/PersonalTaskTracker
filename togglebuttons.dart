82f8584f885c3fffd684f37c1094bec91cc9507d
SawyerTheHollow <115135320+SawyerTheHollow@users.noreply.github.com>
2025-11-23 16:08:47 +0700

    Update togglebuttons.dart

--- togglebuttons.dart
+++ togglebuttons.dart
@@ -2,21 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:stripes_mobile_app/shared/consts/theme/colors.dart';
 
 class PrimaryToggleButtons extends StatefulWidget {
-  final int selectedValue;
+  final ValueNotifier<int> selectedValue;
   final Function(int) onValueChanged;
 
   ///Если не указать высоту и/или ширину, элементы будут пытаться растянуться на всё доступное им пространство по высоте или ширине соответственно.
-  ///Особое внимание уделить высоте. При выбранной горизонтальной прокрутке в не ограниченной по высоте области, виджет упадёт
+  ///Особое внимание уделить высоте. При выбранной горизонтальной прокрутке в не ограниченной по высоте области, виджет упадёт.
   final double? height;
   final double? width;
 
   final BorderRadiusGeometry? borderRadius;
   final double startPadding;
   final double spacing;
-  final List<Widget> widgetList;
+  final ValueNotifier<List<Widget>> widgetList;
   final Color? selectedColor;
   final Color? unselectedColor;
   final Axis scrollDirection;
+  final ScrollPhysics? scrollPhysics;
   final Widget? actionWidget;
 
   ///Начальное значение индеска первой кнопки. Нужно использовать, если на экране есть несколько PrimaryToggleButtons и они используются для
@@ -36,6 +37,7 @@ class PrimaryToggleButtons extends StatefulWidget {
     this.selectedColor = AppColors.blue900,
     this.unselectedColor = AppColors.shade100,
     this.scrollDirection = Axis.horizontal,
+    this.scrollPhysics,
     this.actionWidget,
     this.startWithIndex = 0
   });
@@ -45,63 +47,57 @@ class PrimaryToggleButtons extends StatefulWidget {
 }
 
 class PrimaryToggleButtonsState extends State<PrimaryToggleButtons> {
-  final ValueNotifier<int> _currentValue = ValueNotifier(-1);
-
-  @override
-  void initState() {
-    if (_currentValue.value == -1) {
-      _currentValue.value = widget.selectedValue;
-    }
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
 
-    return Center(
-      child: SizedBox(
+    return SizedBox(
         height: widget.height,
-        child: ListView.builder(
-          shrinkWrap: true,
-          scrollDirection: widget.scrollDirection,
-          itemCount: widget.actionWidget == null ? widget.widgetList.length : widget.widgetList.length + 1,
-          itemBuilder: (context, index) {
-            if(widget.actionWidget != null){
-              return index < widget.widgetList.length ? _statefulButton(index) : widget.actionWidget;
-            }
-            return _statefulButton(index);
+        child: ValueListenableBuilder(
+          valueListenable: widget.widgetList,
+          builder: (context, value, child) {
+            return ListView.builder(
+              physics: widget.scrollPhysics,
+              shrinkWrap: true,
+              scrollDirection: widget.scrollDirection,
+              itemCount: widget.actionWidget == null ? widget.widgetList.value.length : widget.widgetList.value.length + 1,
+              itemBuilder: (context, index) {
+                if(widget.actionWidget != null){
+                  return index < widget.widgetList.value.length ? _statefulButton(index) : widget.actionWidget;
+                }
+                return _statefulButton(index);
+              },
+            );
           },
         ),
-      ),
-    );
+      );
   }
 
   Widget _statefulButton(int index) {
     return ValueListenableBuilder(
-      valueListenable: _currentValue,
+      valueListenable: widget.selectedValue,
       builder: (context, value, child) {
         return Padding(
           padding:
-              //при горизонтальной прокрутке
+          //при горизонтальной прокрутке
           widget.scrollDirection == Axis.horizontal ? EdgeInsets.only(
             left: index == 0 ? widget.startPadding : widget.spacing,
           ) :
-              //при вертикальной прокрутке
+          //при вертикальной прокрутке
           EdgeInsets.only(
             top: index == 0 ? widget.startPadding : widget.spacing,
           ),
           child: GestureDetector(
             onTap: () {
-              _currentValue.value = index + widget.startWithIndex;
-              widget.onValueChanged(_currentValue.value);
+              widget.selectedValue.value = index + widget.startWithIndex;
+              widget.onValueChanged(widget.selectedValue.value);
               //todo убрать это, сейчас удобно для дебага
-              print('текущее выбранное значение: ${_currentValue.value}');
+              print('текущее выбранное значение: ${widget.selectedValue.value}');
             },
             child: AnimatedSwitcher(
               duration: const Duration(milliseconds: 300),
               transitionBuilder: (child, animation){
                 return FadeTransition(opacity: animation,
-                child: child);
+                    child: child);
               },
               child: Container(
                 key: ValueKey(value == index + widget.startWithIndex),
@@ -111,12 +107,12 @@ class PrimaryToggleButtonsState extends State<PrimaryToggleButtons> {
                 padding: const EdgeInsets.all(10),
                 decoration: BoxDecoration(
                   color:
-                      value == index + widget.startWithIndex
-                          ? widget.selectedColor
-                          : widget.unselectedColor,
+                  value == index + widget.startWithIndex
+                      ? widget.selectedColor
+                      : widget.unselectedColor,
                   borderRadius: widget.borderRadius,
                 ),
-                child: widget.widgetList[index],
+                child: widget.widgetList.value[index],
               ),
             ),
           ),
@@ -125,3 +121,5 @@ class PrimaryToggleButtonsState extends State<PrimaryToggleButtons> {
     );
   }
 }
+
+
